e2723326e35712e4ee3afff64fc4355cd8716ef7
Ubuntu <ubuntu@ip-172-31-91-123.ec2.internal>
2020-08-19 15:12:55 +0000

    Added in adding.py

---
+++ adding.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by Marcus A. Mosley
+# Created on August 2020
+# This program adds two user inputted integers
+
+
+def main():
+    # This function calculates total from subtotal and tax
+
+    # Input
+    variable_x = float(input("Enter the first integer to be added: "))
+    variable_y = float(input("Enter the second integer to be added: "))
+
+    # Process
+    total = variable_x + variable_y
+
+    # Output
+    print("")
+    print("The equation is {0} + {1} = {2}"
+          .format(variable_x, variable_y, total))
+
+
+if __name__ == "__main__":
+    main()
